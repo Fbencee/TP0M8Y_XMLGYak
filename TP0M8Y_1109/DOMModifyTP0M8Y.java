e8bafcec186896c874bc3938b6a35a004be67a87
Fbencee <79247037+Fbencee@users.noreply.github.com>
2022-11-15 17:11:09 +0100

    Add files via upload

---
+++ TP0M8Y_1109/DOMModifyTP0M8Y.java
@@ -0,0 +1,82 @@
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class DOMModifyTP0M8Y {
+
+    public static void main(String argv[]) {
+
+        try {
+
+            File inputFile = new File("carsTP0M8Y.xml");
+
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+
+            Document doc = documentBuilder.parse(inputFile);
+
+            Node cars = doc.getFirstChild();
+            Node supercar = doc.getElementsByTagName("supercars").item(0);
+
+            NamedNodeMap attr = supercar.getAttributes();
+            Node nodeAttr = attr.getNamedItem("company");
+            nodeAttr.setTextContent("Lamborghini");
+
+
+            NodeList list = supercar.getChildNodes();
+
+            for (int i = 0; i < list.getLength(); i++) {
+                Node node = list.item(i);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) node;
+
+                    if ("carname".equals(eElement.getNodeName())) {
+                        if ("Ferrari 01".equals(eElement.getTextContent())) {
+                            eElement.setTextContent("Lamborghini 01");
+                        }
+                        if ("Ferrari 02".equals(eElement.getTextContent())) {
+                            eElement.setTextContent("lamborghini 02");
+                        }
+                    }
+                }
+            }
+            NodeList childNodes = cars.getChildNodes();
+
+            for (int j = 0; j < childNodes.getLength(); j++) {
+                Node node = childNodes.item(j);
+
+                if ("luxurycars".equals(node.getNodeName())) {
+                    cars.removeChild(node);
+                }
+            }
+
+            // Kiírás
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+
+            DOMSource source = new DOMSource(doc);
+
+            System.out.println("-----Módosított fájl-----");
+            StreamResult consoleResult = new StreamResult(System.out);
+            transformer.transform(source, consoleResult);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
